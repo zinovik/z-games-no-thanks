e2c0e268a1f428ef63dba55eb81e76f4a1ed9e2b
Max Zinovik <zinovik@gmail.com>
2019-05-22 11:50:50 +0300

    fix getOptionsVariants method

--- src/constants.ts
+++ src/constants.ts
@@ -1,7 +1,7 @@
 export const NAME = 'No, Thanks!';
 export const NAME_WORK = 'no-thanks';
 
-export const PLAYERS_MIN = 1; // TODO: 3
+export const PLAYERS_MIN = 3;
 export const PLAYERS_MAX = 5;
 
 export const MIN_NUMBER = 3;
